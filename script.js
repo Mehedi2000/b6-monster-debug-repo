9ac5a99aa0573f8f453148db7e3aa9e5bfaedeec
Mehedi2000 <mh4054528@gmail.com>
2022-09-15 01:22:10 +0600

    added typing start

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ const start = () => {
 
     // finished timer
     if (count == 0) {
-      countdownOverlay.classList.add('')
+      countdownOverlay.classList.add('d-none')
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "flex";
